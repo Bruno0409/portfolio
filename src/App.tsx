246933bd8bd812fd118515799166220d58b10903
bruno0409 <brunooliveiradesign@hotmail.com>
2025-07-21 19:53:35 -0300

    configurando o ambiente de desenvolvimento

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import EstiloGlobal from './styles.ts'
+import Teste from './Teste.tsx'
+
+function App() {
+  return (
+    <>
+      <EstiloGlobal />
+      <Teste />
+    </>
+  )
+}
+
+export default App
